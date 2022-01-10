1c1577bc221bfc709fe333a1e14e55b679913ff9
fora-heims <foraheims@gmail.com>
2022-01-10 11:58:06 -0800

    files created for Auth view and AuthForm component. BrowserRouter with Switch and Route setup on App.js

---
+++ src/Views/Auth/Auth.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Auth.css';
+
+export default function Auth() {
+  return <div></div>;
+}
---
+++ src/components/AuthForm/AuthForm.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './AuthForm.css';
+
+export default function AuthForm() {
+  return <div></div>;
+}
