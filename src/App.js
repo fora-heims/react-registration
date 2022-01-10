895c3e2d8f8f3be7d87c32978d073c398983e2e4
fora-heims <foraheims@gmail.com>
2022-01-10 12:15:39 -0800

    setup App.js (home) to determine if there is a user logged in. If so it will display a message and a button to logout (which has been setup to call the logout function and set user state to null). If not logged in, the Auth view component will be mounted.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Auth from './Views/Auth/Auth.js';
+import { useState } from 'react';
+import { getUser, logout } from './services/users.js';
 
 function App() {
+  const [user, setUser] = useState(getUser());
+  const handleLogout = async () => {
+    await logout();
+    setUser(null);
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Auth />
+            {user ? (
+              <>
+                <h1>You are logged in</h1>
+                <button onClick={handleLogout}>Log Out</button>
+              </>
+            ) : (
+              <Auth />
+            )}
           </Route>
         </Switch>
       </BrowserRouter>
